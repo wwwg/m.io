6e2f02f48395bad6cff0df14919a6cf91b028596
gotzathe <lolurbadm9@gmail.com>
2017-04-17 20:51:30 -0500

    Modify player teams upon clan add/remove

--- src/entities/Clan.js
+++ src/entities/Clan.js
@@ -2,6 +2,7 @@ class Clan {
     removePlayer(player) {
         for (var i = 0; i < this.members.length; ++i) {
             if (this.members[i].player.sid === player.player.sid) {
+                this.members[i].player.team = null;
                 this.members.splice(i, 1);
                 return true;
             }
@@ -9,6 +10,7 @@ class Clan {
         return false;
     }
     addPlayer(player) {
+        player.player.team = this.name;
         this.members.push(player);
     }
     constructor(name) {
