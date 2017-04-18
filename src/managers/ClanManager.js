00882a389686542ae8d4bd520b2b7ca8553d43d5
gotzathe <lolurbadm9@gmail.com>
2017-04-17 20:45:29 -0500

    Add clan manager methods

--- src/managers/ClanManager.js
+++ src/managers/ClanManager.js
@@ -1,6 +1,25 @@
 var Clan = require("../entities/Clan");
 
 class ClanManager {
+    clanExists(name) {
+        for (var i = 0; i < this.clans.length; ++i) {
+            if (this.clans[i].name === name)
+                return true;
+        }
+        return false;
+    }
+    add(clanName) {
+        this.clans.push(new Clan(clanName));
+    }
+    remove(clanName) {
+        for (var i = 0; i < this.clans.length; ++i) {
+            if (this.clans[i].name === clanName) {
+                this.clans.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
     connstructor(gameServer) {
         this.gameServer = gameServer;
         this.clans = [];
