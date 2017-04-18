4e587a8f36bd02b8f790d16669eadefd503e81c4
gotzathe <lolurbadm9@gmail.com>
2017-04-17 20:46:49 -0500

    Add clan methods

--- src/entities/Clan.js
+++ src/entities/Clan.js
@@ -1,4 +1,16 @@
 class Clan {
+    removePlayer(player) {
+        for (var i = 0; i < this.members.length; ++i) {
+            if (this.members[i].player.sid === player.player.sid) {
+                this.members.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+    addPlayer(player) {
+        this.members.push(player);
+    }
     constructor(name) {
         if (name) {
             this.name = name;
