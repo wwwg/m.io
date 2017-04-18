f07c5b1510138ee49a159b1fb7d4e10378076c29
gotzathe <lolurbadm9@gmail.com>
2017-04-17 19:27:02 -0500

    Update packet codes

--- src/utils/packetCodes.js
+++ src/utils/packetCodes.js
@@ -4,9 +4,10 @@ module.exports = {
     PLAYER_ADD: "2",
     PLAYER_ANGLE: "2",
     PLAYER_UPDATE: "3",
+    PLAYER_MOVE: "3",
     PLAYER_REMOVE: "4",
     LEADERS_UPDATE: "5",
-    LOAD_GAMEOBJ: "6",
+    LOAD_GAME_OBJ: "6",
     GATHER: "7",
     WIGGLE: "8"
 }
