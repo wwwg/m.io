fe77b6acb7b6484bbea8e36cf8781f9eee6fab99
gotzathe <gotzathe@gmail.com>
2017-04-23 02:02:44 -0500

    Add Sword class

---
+++ src/weapon/Sword.js
@@ -0,0 +1,12 @@
+var Weapons = require('./Weapons');
+class Sword extends Weapons.Weapon {
+	constructor() {
+		super(0);
+		this.setEffect(() => {
+			this.weaponCode = 1;
+			this.gatherRate = 2;
+			this.damage = 15;
+		});
+	}
+}
+module.exports = Sword;
