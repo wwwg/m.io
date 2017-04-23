42022a2fd283e9edf25dd5f1244f1362ca2a59d7
gotzathe <gotzathe@gmail.com>
2017-04-23 01:57:21 -0500

    Add Hammer class

---
+++ src/weapon/Hammer.js
@@ -0,0 +1,11 @@
+var Weapons = require('./Weapons');
+class Hammer extends Weapons.Weapon {
+	constructor() {
+		super(0);
+		this.setEffect(() => {
+			this.weaponCode = 0;
+			this.gatherRate = 2;
+			this.damage = 5;
+		});
+	}
+}
