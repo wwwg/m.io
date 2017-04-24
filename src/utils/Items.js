fe12c745841ee7f81a9736da54c1ee0c060e5c16
gotzathe <gotzathe@gmail.com>
2017-04-23 20:31:42 -0500

    Remove debug logging

--- src/utils/Items.js
+++ src/utils/Items.js
@@ -18,7 +18,6 @@ class Item {
 var items = [];
 items[0] = new Item(0, me => { // Apple
 	var heal = 20;
-	console.log(me.health);
 	if (me.health === me.maxHealth) {
 		return;
 	} else if (me.health + heal > me.maxHealth) {
@@ -28,7 +27,6 @@ items[0] = new Item(0, me => { // Apple
 		me.health += heal;
 	}
 	me.buildCode = -1;
-	console.log(me.health);
 }, false);
 items[0].costs.food = 20;
 
